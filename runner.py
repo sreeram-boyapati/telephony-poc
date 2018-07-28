3fcdeadf7554cc3b3da1942f4952793bc0448c59
Sreeram Boyapati <sreeram.boyapati2011@gmail.com>
2018-07-28 19:52:30 +0530

    Use a different bind address

--- runner.py
+++ runner.py
@@ -1,5 +1,11 @@
+import os
+
 from plivo.app import app
 
 from plivo import routes
 
-app.run(host=app.config['SERVER_NAME'], port=5000)
+if app.config['DEBUG'] == False:
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+elif app.config['DEBUG'] == True:
+    app.run(host='127.0.0.1', port=5000)
